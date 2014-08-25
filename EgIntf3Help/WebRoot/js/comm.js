740985eedac65bf24f957b58c9b94d4f4c1c55c1
michaelei <happyjeef18@163.com>
2014-08-25 15:00:37 +0800

    Revert "create"
    This reverts commit 1ab40cd57c83bbf44910ba34436cca82eb80ce73.

--- EgIntf3Help/WebRoot/js/comm.js
+++
@@ -1,8 +0,0 @@
-// JavaScript Document
-var fnParentToPage = function(url){
-	if(window.parent == null)
-		return;
-	if(window.parent.fnIframeToPage == null)
-		return;
-	window.parent.fnIframeToPage(url);
-};
--- EgIntf3Help/WebRoot/js/index.js
+++
@@ -1,8 +0,0 @@
-//使用js来控制iframe的跳转
-var fnIframeToPage = function(url){
-	$('#iframe_center').attr('src',url);
-};
-
-var fnInfo = function(){
-	alert('英杰无线门锁接口(EgIntf3)API说明  测试');
-};
